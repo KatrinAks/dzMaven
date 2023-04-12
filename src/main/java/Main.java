85b18a7dc8aff82526713775cd1ca92bec9f1117
Katrin <e_aksenova80@mail.ru>
2023-04-12 14:29:27 +0300

    Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        BonusService service = new BonusService();
 
+        long expected = 30;
+        long actual =(int) service.calculate(1000, true);
+        System.out.println("1." + expected + " == ? == " + actual);
+
+        expected = 10;
+        actual = (int) service.calculate(1000, false);
+        System.out.println("2." + expected + " == ? == " + actual);
 
     }
 }
